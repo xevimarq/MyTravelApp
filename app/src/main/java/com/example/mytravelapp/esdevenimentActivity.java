9c5f1db0bc571bc98124e8af49d5f7d34ef5b436
Laura <lbujalance02@gmail.com>
2023-05-27 23:25:44 +0200

    Esdeveniments marcats si li dones click et porta al viatje

--- app/src/main/java/com/example/mytravelapp/esdevenimentActivity.java
+++ app/src/main/java/com/example/mytravelapp/esdevenimentActivity.java
@@ -3,10 +3,9 @@ package com.example.mytravelapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.CalendarView;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -16,6 +15,12 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -27,12 +32,12 @@ import java.util.Locale;
 
 public class esdevenimentActivity extends AppCompatActivity {
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
     TextView tripInfoText;
-    CalendarView calendarView;
+    MaterialCalendarView calendarView;
     Controller controller;
     FirebaseFirestore db;
     ArrayList<String> tripDates;
+    private int index = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,34 +45,59 @@ public class esdevenimentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_esdeveniment);
         controller = Controller.getInstance();
         tripInfoText = findViewById(R.id.trip_info_text);
-        calendarView = findViewById(R.id.calendar);
+        calendarView = findViewById(R.id.calendarView);
 
         db = FirebaseFirestore.getInstance();
         retrieveTripDates();
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+        DayViewDecorator tripDateDecorator = new DayViewDecorator() {
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                String selectedDate = formatDate(year, month, dayOfMonth);
-                if (tripDates.contains(selectedDate)) {
-                    int index = tripDates.indexOf(selectedDate);
+            public boolean shouldDecorate(CalendarDay day) {
+                Date date = day.getDate();
+                String formattedDate = formatDate(date);
+                return tripDates.contains(formattedDate);
+            }
+
+            @Override
+            public void decorate(DayViewFacade view) {
+                view.addSpan(new DotSpan(5, Color.RED));
+            }
+        };
+        calendarView.addDecorator(tripDateDecorator);
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(MaterialCalendarView widget, CalendarDay date, boolean selected) {
+                Date selectedDate = date.getDate();
+                String formattedDate = formatDate(selectedDate);
+                if (tripDates.contains(formattedDate)) {
+                    index = tripDates.indexOf(formattedDate);
                     String tripName = controller.getTripName(index);
+                    controller.setViatjeActual(index);
                     tripInfoText.setText(tripName);
                 } else {
                     tripInfoText.setText("No hay viaje en esa fecha");
                 }
             }
         });
-    }
-    private void retrieveTripDates() {
-        tripDates = controller.getTripDate();
-        Log.w("aaa",tripDates.get(1));
+        tripInfoText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Viatje viatje = controller.getViatjeActual();
+                Intent cambiarActivitat = new Intent(esdevenimentActivity.this, viatjeActivity.class);
+                cambiarActivitat.putExtra("tripName", viatje.getNom());
+                startActivity(cambiarActivitat);
+
+            }
+        });
     }
 
-    private String formatDate(int year, int month, int dayOfMonth) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, dayOfMonth);
+    private void retrieveTripDates() {
+        tripDates = controller.getTripDate();
+    }
+
+    private String formatDate(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-        return sdf.format(calendar.getTime());
+        return sdf.format(date);
     }
 
     public void profileButtonClick(View view) {
